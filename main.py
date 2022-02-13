41eee5d406f2c8ab8e83025067197128a44570d8
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2022-02-13 23:41:35 +0900

    C:/Program Files/Git/questionを実装

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, make_response
+from flask import Flask, render_template, jsonify, make_response, request
 import requests
 import json
 import os
@@ -23,19 +23,23 @@ def start():
 
 @app.route('/question', methods=['GET','POST'])
 def question():
-    if not requests.json:
+    if not request.json:
         return make_response('', 400)
-    body = requests.json
+    body = request.json
     game_id = body['game_id']
+    if not game_id:
+        return make_response('auth error', 400)
     question_id = body['question_id']
+    instance = QuestionDAO()
+    game_id = instance.find_question(question_id)
     return jsonify({'game_id': game_id, 'question_id': question_id})
-
+# curl -X POST -H "Content-Type: application/json" -d '{"question_id":"abfb4da9-fcdb-4951-943f-5d483b079e57", "game_id":"1"}' http://localhost:5000/question
 
 @app.route('/question/answer', methods=['GET','POST'])
 def question_answer():
-    if not requests.json:
+    if not request.json:
         return make_response('', 400)
-    body = requests.json
+    body = request.json
     game_id = body['game_id']
     question_id = body['question_id']
     result = body['result']
@@ -44,17 +48,17 @@ def question_answer():
 
 @app.route('/result', methods=['GET','POST'])
 def result():
-    if not requests.json:
+    if not request.json:
         return make_response('', 400)
-    body = requests.json
+    body = request.json
     game_id = body['game_id']
     return jsonify({'game_id': game_id})
 
 @app.route('/end', methods=['GET','POST'])
 def end():
-    if not requests.json:
+    if not request.json:
         return make_response('', 400)
-    body = requests.json
+    body = request.json
     game_id = body['game_id']
     return jsonify({'game_id': game_id})
 
