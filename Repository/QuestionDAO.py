063c21b4d015acf9fafee5f0643b9105363ee7b4
KazukiOtomo <67196994+KazukiOtomo@users.noreply.github.com>
2022-02-15 01:31:21 +0900

    get_game_idの修正

--- Repository/QuestionDAO.py
+++ Repository/QuestionDAO.py
@@ -17,20 +17,23 @@ class QuestionDAO:
     def get_game_id(self):
         conn = sqlite3.connect('sample.db')
         c = conn.cursor()
-        # UUIDが被らなくなるまでループ
         while(True):
             # UUIDの発行(ランダム)
             game_id = str(uuid.uuid4())
-            dupli_flag = c.execute(f"select * from answer_table where game_id = '{game_id}'")
-            # SELECE文に引っかからない ＝ dupli_flagはNone
-            if dupli_flag.fetchone() == None:
+
+            # dupli_flag:既存＝1~ 、被りなし＝0
+            dupli_flag = c.execute(f"select count (game_id) from answer_table where game_id = '{game_id}'")
+
+            if dupli_flag.fetchone()[0] == 0:
                 break
+
         conn.close()
         return game_id
 
     """
     2度目以降用のINSERTメソッド
     """
+
 
     def insert_answer(self, game_id, question_number, answer_flag):
         conn = sqlite3.connect('sample.db')
@@ -39,9 +42,12 @@ class QuestionDAO:
         conn.commit()
         conn.close()
 
+
     """
     後に利用するメソッド
     """
+
+
     # dict_factoryの定義
     def dict_factory(self, cursor, row):
         d = {}
@@ -49,9 +55,11 @@ class QuestionDAO:
             d[col[0]] = row[idx]
         return d
 
+
     """
     質問番号から質問内容を引き出すメソッド
     """
+
 
     # return dict [ {'question_id': 質問番号, 'question': 質問内容'},{......}]
     def find_question(self, question_number):
@@ -66,11 +74,13 @@ class QuestionDAO:
         conn.close()
         return question_dict
 
+
     """
     引数の　game_idから、answer_tableの内容を検索してソートした後に、辞書型に変換して返す（find_questionを参考にどうぞ）
     @:param game_id
     @:return answer_dict
     """
+
 
     def find_answer(self, game_id):
         conn = sqlite3.connect('sample.db')
@@ -81,6 +91,7 @@ class QuestionDAO:
         answer_dict = c.fetchall()
         conn.close()
         return answer_dict
+
 
 instance = QuestionDAO()
 print(instance.get_game_id())
