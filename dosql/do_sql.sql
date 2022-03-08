a57326e61ddf0c3022630ffa60343ac0f1455bc8
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2022-03-08 16:20:45 +0900

    sqlを実行

--- dosql/do_sql.sql
+++ dosql/do_sql.sql
@@ -8,6 +8,20 @@
 --     circle_description varchar(256)
 -- );
 
-insert into circle_list (circle_id, circle_name, circle_image_url, circle_description)
-values (0, 'LTサークル', 'https://cist-lt-group.web.app/static/media/logo.436cc4cb.svg', 'IT技術系の勉強をしています！初心者大歓迎です！所属メンバーはバイオ系、電子工学系、情報工学系と様々なメンバーで構成されています！！みんなで興味のあることを勉強し、アウトプットすることを目標にしています！一人で悩まないで！一緒に技術力を高めませんか？？');
+-- insert into circle_list (circle_id, circle_name, circle_image_url, circle_description)
+-- values (0, 'LTサークル', 'https://cist-lt-group.web.app/static/media/logo.436cc4cb.svg', 'IT技術系の勉強をしています！初心者大歓迎です！所属メンバーはバイオ系、電子工学系、情報工学系と様々なメンバーで構成されています！！みんなで興味のあることを勉強し、アウトプットすることを目標にしています！一人で悩まないで！一緒に技術力を高めませんか？？');
 
+insert into circle_list (circle_id, circle_name, circle_image_url, circle_description)
+values (1, 'Unityサークル', 'https://cist-lt-group.web.app/static/media/logo.436cc4cb.svg', 'IT技術系の勉強をしています！初心者大歓迎です！所属メンバーはバイオ系、電子工学系、情報工学系と様々なメンバーで構成されています！！みんなで興味のあることを勉強し、アウトプットすることを目標にしています！一人で悩まないで！一緒に技術力を高めませんか？？');
+insert into circle_list (circle_id, circle_name, circle_image_url, circle_description)
+values (2, '麻雀サークル', 'https://cist-lt-group.web.app/static/media/logo.436cc4cb.svg', 'IT技術系の勉強をしています！初心者大歓迎です！所属メンバーはバイオ系、電子工学系、情報工学系と様々なメンバーで構成されています！！みんなで興味のあることを勉強し、アウトプットすることを目標にしています！一人で悩まないで！一緒に技術力を高めませんか？？');
+insert into circle_list (circle_id, circle_name, circle_image_url, circle_description)
+values (3, 'プログラミングサークル', 'https://cist-lt-group.web.app/static/media/logo.436cc4cb.svg', 'IT技術系の勉強をしています！初心者大歓迎です！所属メンバーはバイオ系、電子工学系、情報工学系と様々なメンバーで構成されています！！みんなで興味のあることを勉強し、アウトプットすることを目標にしています！一人で悩まないで！一緒に技術力を高めませんか？？');
+insert into circle_list (circle_id, circle_name, circle_image_url, circle_description)
+values (4, 'カーリングサークル', 'https://cist-lt-group.web.app/static/media/logo.436cc4cb.svg', 'IT技術系の勉強をしています！初心者大歓迎です！所属メンバーはバイオ系、電子工学系、情報工学系と様々なメンバーで構成されています！！みんなで興味のあることを勉強し、アウトプットすることを目標にしています！一人で悩まないで！一緒に技術力を高めませんか？？');
+insert into circle_list (circle_id, circle_name, circle_image_url, circle_description)
+values (5, 'カバディサークル', 'https://cist-lt-group.web.app/static/media/logo.436cc4cb.svg', 'IT技術系の勉強をしています！初心者大歓迎です！所属メンバーはバイオ系、電子工学系、情報工学系と様々なメンバーで構成されています！！みんなで興味のあることを勉強し、アウトプットすることを目標にしています！一人で悩まないで！一緒に技術力を高めませんか？？');
+insert into circle_list (circle_id, circle_name, circle_image_url, circle_description)
+values (6, 'トライアスロン部', 'https://cist-lt-group.web.app/static/media/logo.436cc4cb.svg', 'IT技術系の勉強をしています！初心者大歓迎です！所属メンバーはバイオ系、電子工学系、情報工学系と様々なメンバーで構成されています！！みんなで興味のあることを勉強し、アウトプットすることを目標にしています！一人で悩まないで！一緒に技術力を高めませんか？？');
+insert into circle_list (circle_id, circle_name, circle_image_url, circle_description)
+values (7, '鳥人間サークル', 'https://cist-lt-group.web.app/static/media/logo.436cc4cb.svg', 'IT技術系の勉強をしています！初心者大歓迎です！所属メンバーはバイオ系、電子工学系、情報工学系と様々なメンバーで構成されています！！みんなで興味のあることを勉強し、アウトプットすることを目標にしています！一人で悩まないで！一緒に技術力を高めませんか？？');
